c7cfaa84886bb68b6e831e8249e07318d846956e
Joshua <eichler.summers@gmail.com>
2013-03-20 12:40:19 +0000

    trying to fix JQ

--- ShufflehubJQ.js
+++ ShufflehubJQ.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 	var images = [ 'image2.jpg', 'image4.jpg', 'image5.jpg', 'image6.jpg', 'image7.jpg'];
 
 	$('#random').click(function(){
-		$('html').css({'background-image': 'url(images/' + images[Math.floor(Math.random() * images.length)] + ')'});
+		$('html').css('background-image': 'url(images/' + images[Math.floor(Math.random() * images.length)] + ')');
 	});
 
 };
